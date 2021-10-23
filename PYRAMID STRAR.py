e54100b87fe2f74194b9bd7ea26a7efc0c91a9ad
AmanMuricken <89331271+AmanMuricken@users.noreply.github.com>
2021-10-23 10:43:38 +0530

    Add files via upload

---
+++ PYRAMID STRAR.py
@@ -0,0 +1,9 @@
+def pyramid_star(n):
+    for i in range(0,n):
+        for j in range(0,i+1):
+            print("*",end="")
+        print("\r")
+
+n = int(input("Enter a value:"))
+pyramid_star(n)
+    
